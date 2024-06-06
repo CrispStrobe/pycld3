8f0fa2cfc8924da13177298b356bf66315ddcf2f
CrispStrobe <154636388+CrispStrobe@users.noreply.github.com>
2024-06-06 10:12:18 +0200

    compatibility with recent macos brew etc

--- setup.py
+++ setup.py
@@ -8,22 +8,20 @@ from distutils.command.build import build
 from os import makedirs, path
 
 from setuptools import Extension, setup
+from Cython.Build import cythonize
 
 use_cython_val = os.environ.get("USE_CYTHON")
 USE_CYTHON = use_cython_val is not None and use_cython_val not in ("0", "false", "False")
 
-if USE_CYTHON:
-    try:
-        from Cython.Build import cythonize
-    except ImportError as e:
-        # ModuleNotFoundError is Python 3.6+
-        # If user explicitly specifies USE_CYTHON=1, they need Cython installed prior to build
-        raise RuntimeError("Specified USE_CYTHON=1 but could not find Cython installed") from e
-
 HERE = path.abspath(path.dirname(__file__))
 
+# Resolve dynamic paths
+brew_prefix = subprocess.check_output(['brew', '--prefix']).decode().strip()
+protobuf_include = subprocess.check_output(['brew', '--prefix', 'protobuf']).decode().strip() + '/include'
+abseil_include = subprocess.check_output(['brew', '--prefix', 'abseil']).decode().strip() + '/include'
+protobuf_lib = subprocess.check_output(['brew', '--prefix', 'protobuf']).decode().strip() + '/lib'
+
 # List of source filenames, relative to the distribution root
-# (where the setup script lives)
 SOURCES = [
     "src/base.cc",
     "src/cld_3/protos/feature_extractor.pb.cc",
@@ -54,13 +52,6 @@ SOURCES = [
     "src/workspace.cc",
 ]
 
-# Avoid forcing user to have Cython; let them compile the intermediate
-# CPP source file instead
-# https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#distributing-cython-modules
-#
-# Note that both of these (most notably the Cython-generate .cpp file) must be included
-# in the sdist at distribution generation time! The conditional logic here applies to the installing
-# user and what they choose to build the extension from. Both files must be available in the sdist.
 if USE_CYTHON:
     SOURCES.insert(0, "cld3/pycld3.pyx")
 else:
@@ -71,40 +62,46 @@ INCLUDES = [
     "/usr/local/include/",
     path.join(HERE, "src/"),
     path.join(HERE, "src/cld_3/protos/"),
+    f"{brew_prefix}/opt/python@3.11/Frameworks/Python.framework/Versions/3.11/include/python3.11/",
+    path.join(HERE, "src"),
+    protobuf_include,  # Protobuf include directory
+    abseil_include,    # Abseil include directory
 ]
 
-# List of library names (not filenames or paths) to link against
+# List of library names to link against
 LIBRARIES = ["protobuf"]
 
-# https://docs.python.org/3/distutils/setupscript.html#describing-extension-modules
+# List of directories to search for libraries
+LIBRARY_DIRS = [
+    protobuf_lib,
+]
+
+# Describe extension modules
 kwargs = dict(
     sources=SOURCES,
     include_dirs=INCLUDES,
     libraries=LIBRARIES,
+    library_dirs=LIBRARY_DIRS,
     language="c++",
 )
 plat = platform.system()
 if plat == "Darwin":
-    kwargs["extra_compile_args"] = ["-std=c++11", "-stdlib=libc++"]
+    kwargs["extra_compile_args"] = ["-std=c++17", "-stdlib=libc++"]
     kwargs["extra_link_args"] = ["-stdlib=libc++"]
 elif plat != "Windows":
-    kwargs["extra_compile_args"] = ["-std=c++11"]
+    kwargs["extra_compile_args"] = ["-std=c++17"]
 
 ext = [
     Extension("cld3._cld3", **kwargs)
-]  # Name of the extension by which it can be imported
+]
 
 # .proto files define protocol buffer message formats
-# https://developers.google.com/protocol-buffers/docs/cpptutorial
 PROTOS = ["sentence.proto", "feature_extractor.proto", "task_spec.proto"]
-
 
 class BuildProtobuf(build):
     """Compile protocol buffers via `protoc` compiler."""
 
     def run(self):
-
-        # Raise & exit early if `protoc` compiler not available
         if shutil.which("protoc") is None:
             raise RuntimeError(
                 "The Protobuf compiler, `protoc`, which is required for"
@@ -112,27 +109,16 @@ class BuildProtobuf(build):
                 "See https://github.com/protocolbuffers/protobuf for"
                 " information on installing Protobuf."
             )
-
-        # Create protobufs dir if it does not exist
         protobuf_dir = path.join(HERE, "src/cld_3/protos/")
         if not path.exists(protobuf_dir):
             print("Creating dirs at \033[1m{}\033[0;0m".format(protobuf_dir))
             makedirs(protobuf_dir)
-
-        # Run command via subprocess, using protoc compiler on .proto
-        # files
-        #
-        # $ cd src && protoc --cpp-_ut cld_3/protos \
-        # >     sentence.proto feature_extractor.proto task_spec.proto
         command = ["protoc"]
         command.extend(PROTOS)
-        command.append(
-            "--cpp_out={}".format(path.join(HERE, "src/cld_3/protos/"))
-        )
+        command.append("--cpp_out={}".format(path.join(HERE, "src/cld_3/protos/")))
         print("Running \033[1m{}\033[0;0m".format(" ".join(command)))
         subprocess.run(command, check=True, cwd=path.join(HERE, "src/"))
         build.run(self)
-
 
 CLASSIFIERS = [
     "License :: OSI Approved :: Apache Software License",
@@ -150,7 +136,6 @@ CLASSIFIERS = [
     "Intended Audience :: Developers",
 ]
 
-
 def find_version(filepath):
     version = None
     with open(filepath) as f:
@@ -161,15 +146,11 @@ def find_version(filepath):
         raise RuntimeError("Could not find version in __init__.py")
     return version
 
-
 if __name__ == "__main__":
-
-    # https://docs.python.org/3/distutils/setupscript.html#additional-meta-data
     if USE_CYTHON:
-        extensions = cythonize(ext)
+        extensions = cythonize(ext, compiler_directives={'language_level': "3"})
     else:
         extensions = ext
-
     setup(
         name="pycld3",
         version=find_version("cld3/__init__.py"),
@@ -177,9 +158,7 @@ if __name__ == "__main__":
         author="Brad Solomon",
         author_email="bsolomon@protonmail.com",
         description="CLD3 Python bindings",
-        long_description=open(
-            path.join(HERE, "README.md"), encoding="utf-8"
-        ).read(),
+        long_description=open(path.join(HERE, "README.md"), encoding="utf-8").read(),
         long_description_content_type="text/markdown",
         license="Apache 2.0",
         keywords=["cld3", "cffi", "language", "langdetect", "cld", "nlp"],
